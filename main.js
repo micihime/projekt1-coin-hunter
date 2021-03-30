d9ca9f170a1f62a97b3dde7963d9d4554264d803
micihime <hroncovam@yahoo.com>
2021-03-30 16:14:59 +0200

    switch different key codes with console logs

--- main.js
+++ main.js
@@ -8,4 +8,20 @@ if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || pan
 // sem začni psát svůj program
 window.addEventListener('keydown', function (evt) {
 	console.log(evt.keyCode);
+	switch(evt.keyCode) {
+		case 37: //left arrow
+			console.log("left");
+			break;
+		case 38: //up arrow
+			console.log("up");
+			break;
+		case 39: //right arrow
+			console.log("right");
+			break;
+		case 40: //down arrow
+			console.log("down");
+			break;
+		default:
+			console.log("other key")
+	  };
 });
